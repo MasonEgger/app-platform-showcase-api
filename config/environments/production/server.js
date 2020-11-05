21de9a109c3e41e88df7390a647030ad2a7ac9ef
Mason Egger <mason@masonegger.com>
2020-11-05 15:00:25 -0600

    Attempt to fix admin url

--- config/environments/production/server.js
+++ config/environments/production/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
     url: env("STRAPI_URL"),
     port: env.int("PORT", 8080),
     admin: {
+        url: env("STRAPI_URL") + "/admin",
         auth: {
             secret: env("ADMIN_JWT_SECRET"),
         },
